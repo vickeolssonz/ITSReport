47ee00224fb8d85f4428519e344166a819f80ca1
Viktor Olsson <viktor.olsson@nordic.webhelp.com>
2020-05-25 15:27:04 +0200

    Delete button with call to api delete request. Added bootbox to bundle

--- XJob/App_Start/BundleConfig.cs
+++ XJob/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace XJob
 						"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js"));
+				"~/Scripts/bootbox.js",
+			   "~/Scripts/bootstrap.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
